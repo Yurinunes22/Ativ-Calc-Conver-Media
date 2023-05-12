2a9e1a5d9742a46918f5c9eebb959cc69f910250
YURI NUNES DE JESUS <yuri.njesus@senacsp.edu.br>
2023-05-12 20:34:08 -0300

    Criação Desconto Salario

--- PrjDescontoSalario/Form1.cs
+++ PrjDescontoSalario/Form1.cs
@@ -14,32 +14,85 @@ namespace PrjDescontoSalario
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            decimal valorbruto = 0, valorliquido, calc=0, sla;
-
-            valorbruto = Convert.ToDecimal(TxtSalarioBruto.Text);
-
-            if (decimal.TryParse(TxtSalarioBruto.Text, out decimal res) == true)
+            decimal aux1, aux2 = 0, aux3 = 0;
+            if (decimal.TryParse(TxtSalarioBruto.Text, out decimal res) == false)
             {
-                MessageBox.Show("Digite O Valor Do Salário Bruto");
+                TxtSalarioLiquido.Text = "Digite o seu salário!";
             }
-            else if (TxtSalarioBruto.Text == string.Empty)
+            else
             {
-                MessageBox.Show("Digite O Valor Do Salário Bruto");
-            }
-            else if (chkValeTransporte.Checked == true)
-            {
-                calc = valorbruto * 0.06m;
-                
-            }   
-            else if (chkConvenio.Checked == true)
-            {
-                calc = (valorbruto - 20);
+                decimal salbruto = Convert.ToDecimal(TxtSalarioBruto.Text);
 
-            }
-            
-            
+                if (chkValeTransporte.Checked == true && chkConvenio.Checked == false)
+                {
+                    aux2 = salbruto * 0.94m;
 
-            
+
+
+
+                }
+                if (chkConvenio.Checked == true && chkValeTransporte.Checked == false)
+                {
+                    aux2 = salbruto - 20;
+                }
+                if (chkConvenio.Checked == true && chkValeTransporte.Checked == true)
+                {
+
+
+
+                    aux1 = salbruto * 0.94m;
+                    aux2 = aux1 - 20;
+
+                }
+
+
+
+                if (salbruto < 980)
+                {
+                    aux3 = salbruto * 0.08m;
+                    aux3 = aux2 - aux3;
+               
+ }
+                else if (salbruto >= 980 && salbruto < 1450)
+                {
+                    salbruto = salbruto * 0.09m;
+                    aux3 = aux2 - aux3;
+                }
+                else if (salbruto >= 1450 && salbruto < 2200)
+                {
+                    salbruto = salbruto * 0.0m;
+                    aux3 = aux2 - aux3;
+                }
+                else
+                {
+                    salbruto = salbruto * 0.89m;
+                    aux3 = aux2 - aux3
+                }
+
+
+
+
+
+
+
+
+
+                txtResult.Text = aux3.ToString();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
